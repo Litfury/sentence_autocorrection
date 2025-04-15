8da774ba314542121cbb820bfb79d95d3c961727
Litfury <77917299+Litfury@users.noreply.github.com>
2025-04-15 06:29:16 +0530

    plagarism output rollback

--- src/components/InputForPlagarism.jsx
+++ src/components/InputForPlagarism.jsx
@@ -18,7 +18,7 @@ const InputForPlagarism = () => {
     try {
 
       const apiKey = import.meta.env.VITE_API_key;
-      const systemPrompt = "You are an AI-powered Plagarism detector. Your task is to receive text as input and detect plagarism in the content and give an output only in numbers that how much percentage is AI-generated and fetched from how much percentage of the text is written by human. Do not give anything else as an output except these numbers, if unable to process then return all the values as 0. Return it in this sample format 'Plagarised - 72% and Human-content - 28%'";
+      const systemPrompt = "You are an AI-powered Plagarism detector. Your task is to receive text as input and detect plagarism in the content and give an output only in numbers that how much percentage is written by AI, how much percentage is fetched from internet and how much percentage of the text is written by human. Do not give anything else as an output except these numbers, if unable to process then return all the values as 0. Return it in this sample format 'AI-generated - 4%, From-internet - 2% and Human-content - 94%'";
       const sentence = text;
       const genAI = new GoogleGenerativeAI(apiKey);
       const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
